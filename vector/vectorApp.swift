ca1469a50601c790fe43ec8c534b43e1d926d701
yash.rajput.001 <yash.rajput.001@juspay.in>
2026-03-13 00:22:07 +0530

    Initial Commit

---
+++ vector/vectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  vectorApp.swift
+//  vector
+//
+//  Created by Yash Rajput on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct vectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
